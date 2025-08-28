e00e1f3eb93b2e9a6ab108eef8a6431d6bcfbfdf
estherelzek <esther.elzek@inspirehub.net>
2025-08-28 15:21:12 +0300

    Add enhance files

---
+++ HR/Models/LeaveDurationResponse.swift
@@ -0,0 +1,8 @@
+//
+//  LeaveDurationResponse.swift
+//  HR
+//
+//  Created by Esther Elzek on 28/08/2025.
+//
+
+import Foundation
---
+++ HR/ViewModels/LeaveViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LeaveViewModel.swift
+//  HR
+//
+//  Created by Esther Elzek on 28/08/2025.
+//
+
+import Foundation
