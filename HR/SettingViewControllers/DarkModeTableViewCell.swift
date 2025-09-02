73d55eebda4eb0373f72f6c5c3c5684b03a39be9
estherelzek <esther.elzek@inspirehub.net>
2025-09-02 17:03:45 +0300

    Describe what you updated

---
+++ HR/SettingViewControllers/DarkModeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DarkModeTableViewCell.swift
+//  HR
+//
+//  Created by Esther Elzek on 02/09/2025.
+//
+
+import UIKit
+
+class DarkModeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
