822644915da4206866b7541e4ef41ac5b0b1da87
estherelzek <esther.elzek@inspirehub.net>
2025-09-28 13:55:17 +0300

    ✨ Add new function for splash screen

---
+++ HR/TimeOffViewControllers/TypesOfLeavesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TypesOfLeavesCollectionViewCell.swift
+//  HR
+//
+//  Created by Esther Elzek on 28/09/2025.
+//
+
+import UIKit
+
+class TypesOfLeavesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ HR/TypesOfLeavesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TypesOfLeavesViewController.swift
+//  HR
+//
+//  Created by Esther Elzek on 28/09/2025.
+//
+
+import UIKit
+
+class TypesOfLeavesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
