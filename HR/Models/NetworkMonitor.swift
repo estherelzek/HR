479fde5d34c53adc76fd2bec64f52e121bcd5475
estherelzek <esther.elzek@inspirehub.net>
2025-10-05 15:19:13 +0300

    ✨ Add new function for splash screen

--- HR/Models/NetworkMonitor.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkMonitor.swift
-//  HR
-//
-//  Created by Esther Elzek on 01/10/2025.
-//
-
-import Foundation
---
+++ HR/Models/StateType.swift
@@ -0,0 +1,8 @@
+//
+//  StateType.swift
+//  HR
+//
+//  Created by Esther Elzek on 05/10/2025.
+//
+
+import Foundation
